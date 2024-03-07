a78180c1f219d4ae740fa4d444fc8feb1dae1434
Renato Roger <72948533+RenatoX56@users.noreply.github.com>
2024-03-07 10:37:58 -0300

    cone

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'RenatoX56' 
+description 'A cone...'
+
+-- [EN] This is what the forum posts look like lately XD lol
+
+-- [ES] Así se ven las publicaciones del foro últimamente XD
+
+this_is_a_map 'yes'
